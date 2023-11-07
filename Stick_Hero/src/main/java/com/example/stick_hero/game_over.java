6a70460f8d9866b581f02d7faeb1785d1059eccf
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-11-07 17:18:24 +0530

    new files

---
+++ Stick_Hero/src/main/java/com/example/stick_hero/game_over.java
@@ -0,0 +1,10 @@
+package com.example.stick_hero;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class game_over {
+    @FXML
+    ImageView image_view_1;
+    Image hero = new Image(getClass().getResourceAsStream(""));
+}
