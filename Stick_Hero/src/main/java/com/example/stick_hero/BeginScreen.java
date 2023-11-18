f516a22eecfad39714cadd64b6295a66e85fb952
VIRUSXSUJAL69 <prakhar22361@iiitd.ac.in>
2023-11-18 08:59:05 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Stick_Hero/src/main/java/com/example/stick_hero/GameStart.java
    #	Stick_Hero/src/main/resources/com/example/stick_hero/GameStart.fxml

--- Stick_Hero/src/main/java/com/example/stick_hero/BeginScreen.java
+++ Stick_Hero/src/main/java/com/example/stick_hero/BeginScreen.java
@@ -5,5 +5,5 @@ import javafx.event.EventHandler;
 public class BeginScreen {
     public void createProfile(EventHandler event){}
     public void enterProfile(EventHandler event){}
-    public void freePlay(EventHandler event) {}
+    public void exit(EventHandler event) {}
 }
