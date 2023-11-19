fb121812084bb6ada5211655385ad1328e92fba0
VIRUSXSUJAL69 <prakhar22361@iiitd.ac.in>
2023-11-19 16:27:18 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Stick_Hero/src/main/java/com/example/stick_hero/GameStart.java
    #	Stick_Hero/src/main/resources/com/example/stick_hero/GameStart.fxml

--- Stick_Hero/src/main/java/com/example/stick_hero/GameOver.java
+++ Stick_Hero/src/main/java/com/example/stick_hero/GameOver.java
@@ -1,11 +1,17 @@
 package com.example.stick_hero;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 
 public class GameOver extends Menu{
     private int best_score, curr_score;
     private Image home, restart;
+    @FXML
+    private Label currentScore;
+    @FXML
+    private Label bestScore;
 
     public GameOver(int cherries, Image background, int best_score, int curr_score, Image home, Image restart) {
         super(cherries, background);
@@ -22,5 +28,8 @@ public class GameOver extends Menu{
         new Game_screen();
         //to restart the game
     }
-
+    public void displayScore(){
+        currentScore.setText(String.valueOf(curr_score));
+        bestScore.setText(String.valueOf(best_score));
+    }
 }
